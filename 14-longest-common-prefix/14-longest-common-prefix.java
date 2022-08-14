e4319290ae071d264b83dfbc5dc5ac5083feff28
Eklavya <eklavyaprasad1@gmail.com>
2022-08-14 11:11:11 +0530

    Time: 3 ms (35.67%), Space: 42.2 MB (51.70%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -6,24 +6,20 @@ class Solution {
     if(strs.length==0 || strs[0].isEmpty()) return "";
     else if(strs.length==1) return strs[0];
         
-    boolean flag=true,isBreakedAtFirst=false;
+    boolean flag=true;
     int index=0;
         
     while(flag){
         try{
             for(int i=0;i<strs.length-1;i++){
                 if(strs[i].charAt(index)!=strs[i+1].charAt(index)) {
-                    if(i!=0){
-                       // ans.setLength(ans.length() - 1);
-                       return ans.toString(); 
-                    }
+                    if(i!=0) return ans.toString(); 
                     flag=false;
                     break;
                 }
                 if(i==strs.length-2) ans.append(strs[0].charAt(index));
             }
-            index++;
-            
+            index++;    
         }catch(Exception e){
             return ans.toString();
         }
