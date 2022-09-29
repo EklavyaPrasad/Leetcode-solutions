baa5cbf250fba69eddc8f93eaa37c3ef0616a252
Eklavya <eklavyaprasad1@gmail.com>
2022-09-30 00:07:58 +0530

    Time: 25 ms (12.26%), Space: 50.4 MB (36.12%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,31 +1,39 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length/3;
+        System.out.println(n);
+        int n1=0,n2=0,count1=0,count2=0;
         List<Integer> ans=new ArrayList<>();
-           
-//         int majority=0,majority_count=0;
-//         for(Integer it: nums){
-            
-//             if(majority_count==0)
-//                 majority=it;
-            
-//             if(it==majority){
-//                 majority_count+=1;
-//                 if(majority_count>n) ans.add(majority);
-//             }      
-//             else 
-//                 majority_count-=1;
-//         }
-//         return ans;
-        
-        HashMap<Integer,Integer> hs=new HashMap<>();
+          
         for(Integer it: nums){
-            if(hs.containsKey(it)) hs.put(it,hs.get(it)+1);
-            else hs.put(it,1);
+            if(it==n1) 
+                count1++;
+            else if(it==n2) 
+                count2++;
+            else if(count1==0){
+                n1=it;
+                count1=1;
+            }else if(count2==0){
+                n2=it;
+                count2=1;
+            }else{
+                count1--;
+                count2--;
+            }
         }
-        for(Map.Entry<Integer,Integer> entry: hs.entrySet()){
-            if(entry.getValue()>n) ans.add(entry.getKey());
+        System.out.println(n1+" "+n2);
+        count1 = 0;
+        count2 = 0;
+        for(int it : nums) {
+            if(it == n1)
+                count1++;
+            else if(it == n2)
+                count2++;
         }
+        if(count1 > n) ans.add(n1);
+        if(count2 > n) ans.add(n2); 
+        
         return ans;
+        
     }
 }
