7a119f5c99922b43a8c9d04fa9bc3ce79138bf89
Eklavya <eklavyaprasad1@gmail.com>
2022-10-12 09:23:49 +0530

    Time: 96 ms (70.77%), Space: 121.9 MB (25.92%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -4,11 +4,10 @@ class Solution {
         
         HashSet<Integer> hs=new HashSet<>();
         for(;r<nums.length;r++){
-            while(hs.contains(nums[r])){
+            while(!hs.add(nums[r])){
                 sum-=nums[l];
                 hs.remove(nums[l++]);
             }
-            hs.add(nums[r]);
             sum+=nums[r];
             ans=Math.max(ans,sum);
         }
